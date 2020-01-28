96cb01aaa015fb321a5ccc35e88c41120c5582bd
sagar <sagardeshmukh447@gmail.com>
2020-01-28 12:59:15 +0530

    Updated on 28.01.2020 at 12.59

--- app/src/main/java/com/example/choiceproperties/Views/Activities/Main2Activity.java
+++ app/src/main/java/com/example/choiceproperties/Views/Activities/Main2Activity.java
@@ -22,6 +22,7 @@ import com.example.choiceproperties.Views.Fragments.Fragment_Sold_Out_Plots;
 import com.example.choiceproperties.Views.Fragments.Requests__Tab_Fragment;
 import com.example.choiceproperties.Views.Fragments.Sales_Customer_Requests_Fragment;
 import com.example.choiceproperties.interfaces.OnFragmentInteractionListener;
+import com.example.choiceproperties.preferences.AppSharedPreference;
 import com.google.firebase.auth.FirebaseAuth;
 import com.shrikanthravi.customnavigationdrawer2.data.MenuItem;
 import com.shrikanthravi.customnavigationdrawer2.widget.SNavigationDrawer;
@@ -32,6 +33,7 @@ import java.util.List;
 public class Main2Activity extends AppCompatActivity implements OnFragmentInteractionListener {
 
     SNavigationDrawer sNavigationDrawer;
+    private AppSharedPreference appSharedPreference;
     int color1=0;
     Class fragmentClass;
     public static Fragment fragment;
@@ -51,10 +53,11 @@ public class Main2Activity extends AppCompatActivity implements OnFragmentIntera
 //            Toast.makeText(this, "Storage Premission Required", Toast.LENGTH_SHORT).show();
         }
 
+        appSharedPreference = new AppSharedPreference(getApplicationContext());
         sNavigationDrawer = findViewById(R.id.navigationDrawer);
         List<MenuItem> menuItems = new ArrayList<>();
         menuItems.add(new MenuItem("Requests",R.color.Black));
-        menuItems.add(new MenuItem("Add Customers",R.color.Black));
+//        menuItems.add(new MenuItem("Add Customers",R.color.Black));
         menuItems.add(new MenuItem("Add Plots",R.color.Black));
         menuItems.add(new MenuItem("Sale Plot",R.color.Black));
         menuItems.add(new MenuItem("Accept Sales Requests",R.color.Black));
@@ -85,46 +88,48 @@ public class Main2Activity extends AppCompatActivity implements OnFragmentIntera
                         fragmentClass = Requests__Tab_Fragment.class;
                         break;
                     }
+//                    case 1:{
+//                        color1 = R.color.orange;
+//                        fragmentClass = Fragment_Add_Customers.class;
+//
+//                        break;
+//                    }
                     case 1:{
-                        color1 = R.color.orange;
-                        fragmentClass = Fragment_Add_Customers.class;
-
-                        break;
-                    }
-                    case 2:{
                         color1 = R.color.green;
                         fragmentClass = Fragment_Add_Plots.class;
                         break;
                     }
-                    case 3:{
+                    case 2:{
                         color1 = R.color.green;
                         fragmentClass = Fragment_Sale_Plots.class;
                         break;
                     }
-                    case 4:{
+                    case 3:{
                         color1 = R.color.green;
                         fragmentClass = Admin_Sales__Tab_Fragment.class;
                         break;
                     }
-                    case 5:{
+                    case 4:{
                         color1 = R.color.green;
                         fragmentClass = Fragment_Sold_Out_Plots.class;
                         break;
                     }
-                    case 6:{
+                    case 5:{
                         color1 = R.color.green;
                         fragmentClass = Fragment_Reports.class;
                         break;
                     }
-                    case 7:{
+                    case 6:{
                         color1 = R.color.blue;
-                        FirebaseAuth.getInstance().signOut();
-                        finish();
-                        startActivity(new Intent(Main2Activity.this, LoginScreen.class));
+//                        FirebaseAuth.getInstance().signOut();
+//                        finish();
+//                        startActivity(new Intent(Main2Activity.this, LoginScreen.class));
+                        clearDataWithSignOut();
                         break;
                     }
 
                 }
+
                 sNavigationDrawer.setDrawerListener(new SNavigationDrawer.DrawerListener() {
 
                     @Override
@@ -167,6 +172,19 @@ public class Main2Activity extends AppCompatActivity implements OnFragmentIntera
             }
         });
 
+    }
+
+    private void clearDataWithSignOut() {
+        FirebaseAuth.getInstance().signOut();
+        appSharedPreference.clear();
+        logOut();
+    }
+    private void logOut() {
+        Intent intent = new Intent(this, LoginScreen.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
     }
 
     public boolean isStoragePermissionGranted() {
